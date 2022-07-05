08c9422b80ae275a9a9664ef411fa0b18523cc15
crivoid <dmitrii.crivoi@etu.univ-grenoble-alpes.fr>
2022-07-05 11:09:01 +0200

    first commit

---
+++ src/app/shared/modules/MyoArchitecture/components/MyoArchitecture.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+import {Observable} from 'rxjs'
+
+@Component({
+  selector: 'mc-architecture',
+  templateUrl: './MyoArchitecture.component.html',
+  styleUrls: ['./MyoArchitecture.component.scss'],
+})
+export class MyoArchitectureComponent {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://conduit.productionready.io/api',//http://localhost:3000/
+  limit: 10,
+}
